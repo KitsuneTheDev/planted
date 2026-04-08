fc49a4c8a11ac6ec9053516d2000baa3e20ff1be
KitsuneTheDev <163762953+KitsuneTheDev@users.noreply.github.com>
2026-04-08 15:38:37 +0300

    setup
    - cors options is set in vite.config.ts (server configuration with proxy options)

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -8,4 +8,18 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  server: {
+    host: true,
+    port: 5173,
+    watch: {
+      usePolling: true // Require extra computation power
+    },
+    proxy: {
+      '/api': {
+        target: 'http://bff/3001', // target url of the server (in this case it's docker image)
+        changeOrigin: true, // change the origin of host to target url whis is defined above
+        rewrite: (path) => path.replace(/^\/api/, ""), // replace /api... part with an empty string
+      }
+    }
+  }
 })
