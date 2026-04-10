f70d4a5cba198c9b1c338e5779ee53027a57982c
KitsuneTheDev <163762953+KitsuneTheDev@users.noreply.github.com>
2026-04-10 23:54:12 +0300

    update
    - something something

---
+++ frontend/src/myplants/MyPlants.tsx
@@ -0,0 +1,17 @@
+import style from './MyPlants.module.css';
+
+export default function MyPlants() {
+    
+
+    return(
+        <div className={style.myplantsContainer}>
+            <div className={style.contentWrapper}>
+                <h3>Plant Card Here</h3>
+                <h3>Plant Card Here</h3>
+                <h3>Plant Card Here</h3>
+                <h3>Plant Card Here</h3>
+                <h3>Plant Card Here</h3>
+            </div>
+        </div>
+    );
+}
