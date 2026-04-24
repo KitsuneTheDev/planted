0e19af62b18e20d4139a41a821cf0ffd9f5665bf
KitsuneTheDev <163762953+KitsuneTheDev@users.noreply.github.com>
2026-04-24 18:08:45 +0300

    update
    -- layout adjustments

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -1,16 +1,21 @@
+import { useState } from 'react';
 import style from './Layout.module.css';
 // import MyPlants from '../../features/plants/MyPlants.tsx';
 
 export default function Layout() {
 
+    const [asideHidden, setAsideHidden] = useState<boolean>(true);
 
     return(
         <div className={style.layoutContainer}>
+            <div className={style.toggleContainer}>
+                <button onClick={() => setAsideHidden(prev => !prev)}>Plants</button>
+            </div>
             <header role='banner'>
                 <h2>Weather</h2>
                 {/* Weather */}
             </header>
-            <aside>
+            <aside className={asideHidden ? style.hidden : ''}>
                 <h2>My Plants</h2>
                 {/* <MyPlants /> */}
             </aside>
