6419bab4fe040842db971f50dbe551bfbf599e61
KitsuneTheDev <163762953+KitsuneTheDev@users.noreply.github.com>
2026-05-04 22:58:40 +0300

    feat: add sequelize setup

---
+++ bff/src/db/config.ts
@@ -0,0 +1,13 @@
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize(
+    process.env.POSTGRES_DB!,
+    process.env.POSTGRES_USER!,
+    process.env.POSTGRES_PASSWORD,
+   {
+        host: process.env.POSTGRES_HOST!,
+        port: Number.parseInt(process.env.POSTGRES_PORT!),
+        dialect: "postgres",
+        logging: false,
+   }
+);
