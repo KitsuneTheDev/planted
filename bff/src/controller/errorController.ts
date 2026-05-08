2c47a89e883e58198b5a2219e302fca7632e2770
KitsuneTheDev <163762953+KitsuneTheDev@users.noreply.github.com>
2026-05-08 19:48:09 +0300

    update: apply error handler for weather service

--- bff/src/controller/errorController.ts
+++ bff/src/controller/errorController.ts
@@ -1,5 +1,5 @@
 import type { NextFunction, Response, Request } from "express";
-import { AppError } from "../util/AppError";
+import { AppError } from "../error/AppError";
 
 export const globalErrorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
     let statusCode: number = 500;
@@ -7,6 +7,12 @@ export const globalErrorHandler = (err: any, req: Request, res: Response, next: 
     let isOperational = false;
     let message = 'Something went wrong!';
     const isDev = process.env.NODE_ENV === 'development';
+
+    console.log("--- GLOBAL ERROR ---");
+    console.log(`is AppError: `, err instanceof AppError);
+    console.log(err.constructor);
+    console.log(err.name);
+    console.log(err.message);
 
     if(err instanceof AppError) {
         statusCode = err.statusCode;
--- bff/src/route/weather.route.ts
+++ bff/src/route/weather.route.ts
@@ -1,8 +1,9 @@
-import express from "express";
+import express, { type NextFunction } from "express";
 import { getCurrentWeatherData } from "../controller/weather.controller.js";
+import { asyncWrapper } from "../util/asyncWrapper.js"
 
 const router = express.Router();
 
-router.get('/weather/:lat/:lon', getCurrentWeatherData);
+router.get('/weather/:lat/:lon', asyncWrapper(getCurrentWeatherData));
 
 export default router;
