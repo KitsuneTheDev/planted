72106a8799e0435c723f58dfbad1aa7c96fe90d5
KitsuneTheDev <163762953+KitsuneTheDev@users.noreply.github.com>
2026-05-13 21:36:42 +0300

    add: frontend dropdown service

---
+++ frontend/src/services/DropdownService.ts
@@ -0,0 +1,23 @@
+import type { ApiResponse } from "../types/api.type";
+import type { DropdownData } from "@planted/types";
+import { ApiClient } from "./ApiClient";
+
+export class DropdownService {
+    private static client: ApiClient | null = null;
+
+    constructor() {}
+
+    private static getClient(): ApiClient {
+        if(!DropdownService.client) {
+            DropdownService.client = new ApiClient('/api/search');
+        }
+
+        return DropdownService.client;
+    }
+
+    static async getDropdownData(query: string): Promise<ApiResponse<DropdownData[]>> {
+        const client = DropdownService.getClient();
+        const response = await client.get<DropdownData[]>(query);
+        return response;
+    }
+}
--- packages/types/index.ts
+++ packages/types/index.ts
@@ -2,3 +2,4 @@ export type { CurrentWeatherResponse } from "./src/weather.type.ts";
 export type { CurrentQualityResponse } from "./src/quality.type.ts";
 export type { WeatherCombined } from "./src/weatherCombined.type.ts";
 export type { IAppError } from "./src/IAppError.ts";
+export type { DropdownData } from './src/DropdownData.ts';
