134d1226cb47d6795c8f89d7688417b2e92175a6
KitsuneTheDev <163762953+KitsuneTheDev@users.noreply.github.com>
2026-05-28 17:17:05 +0300

    feat: add circular progress bar component

--- frontend/src/features/weather/Weather.tsx
+++ frontend/src/features/weather/Weather.tsx
@@ -1,81 +1,81 @@
-// import style from './Weather.module.css';
-// import { useEffect, useState, type ReactElement } from "react";
-// import { useDispatch, useSelector } from 'react-redux';
-// import type { AppDispatch, RootState } from '../../redux/store'
-// import { getWeatherData } from '../../redux/slices/weatherServiceSlice';
+import style from './Weather.module.css';
+import { useEffect, useState, type ReactElement } from "react";
+import { useDispatch, useSelector } from 'react-redux';
+import type { AppDispatch, RootState } from '../../redux/store'
+import { getWeatherData } from '../../redux/slices/weatherServiceSlice';
 
-// export default function Weather(): ReactElement {
+export default function Weather(): ReactElement {
 
-//     const { weatherServiceData } = useSelector((state: RootState) => state.weatherServiceReducer);
-//     const dispatch = useDispatch<AppDispatch>();
-//     const [isExpanded, setIsExpanded] = useState<boolean>(false);
+    const { weatherServiceData } = useSelector((state: RootState) => state.weatherServiceReducer);
+    const dispatch = useDispatch<AppDispatch>();
+    const [isExpanded, setIsExpanded] = useState<boolean>(false);
 
-//     useEffect(() => {
-//         dispatch(getWeatherData({lat: 40, lon: 50}));   
-//     }, []);
+    useEffect(() => {
+        dispatch(getWeatherData({lat: 40, lon: 50}));   
+    }, []);
 
-//     useEffect(() => {
-//         console.log(weatherServiceData);
-//     }, [weatherServiceData])
+    useEffect(() => {
+        console.log(weatherServiceData);
+    }, [weatherServiceData])
 
-//     const handleToggleClick = () => {
-//         setIsExpanded(prev => !prev);
-//     }
+    const handleToggleClick = () => {
+        setIsExpanded(prev => !prev);
+    }
 
-//     return(
-//         <div className={`${style.weatherContainer} ${style.collapsible}  ${isExpanded ? style.expanded : ''}`}>
-//             <div className={`${style.toggleContainer}`} onClick={handleToggleClick}>
-//                 {isExpanded ? 'Hide Details' : 'See Details'}
-//             </div>
-//             <div className={style.weatherMain}>
-//                 <div className={style.primary}>
-//                     <div className={style.temp}>
-//                         <div className={style.value}>
-//                             <div className={style.data}>
-//                                 {Math.round(weatherServiceData?.weather?.main.temp || 0)}
-//                             </div>
-//                             <div className={style.detail}>
-//                                 &deg;C
-//                             </div>
-//                         </div>
-//                     </div>
-//                 </div>
-//                 <div className={style.secondary}>
-//                     <div className={style.feelslike}>
-//                         <div className={style.label}>
-//                             But you think it is
-//                         </div>
-//                         <div className={style.value}>
-//                             <div className={style.data}>
-//                                 {Math.round(weatherServiceData?.weather?.main.feels_like || 0)}
-//                             </div>
-//                             <div className={style.detail}>
-//                                 &deg;C
-//                             </div>
-//                         </div>
-//                     </div>
-//                     <div className={style.humidity}>
-//                         <div className={style.label}>
-//                             And humidity is
-//                         </div>
-//                         <div className={style.value}>
-//                             <div className={style.data}>
-//                                 {Math.round(weatherServiceData?.weather?.main.humidity || 0)}
-//                             </div>
-//                             <div className={style.detail}>
-//                                 %
-//                             </div>
-//                         </div>
-//                     </div>
-//                 </div>
-//             </div>
-//             <div className={`${style.weatherDetail} ${isExpanded ? style.expanded : ''}`}>
-//                 <div className={style.collapsible__inner}>
-//                     <div>This</div>
-//                     <div>is</div>
-//                     <div>details</div>
-//                 </div>
-//             </div>
-//         </div>
-//     );
-// }
+    return(
+        <div className={`${style.weatherContainer} ${style.collapsible}  ${isExpanded ? style.expanded : ''}`}>
+            <div className={`${style.toggleContainer}`} onClick={handleToggleClick}>
+                {isExpanded ? 'Hide Details' : 'See Details'}
+            </div>
+            <div className={style.weatherMain}>
+                <div className={style.primary}>
+                    <div className={style.temp}>
+                        <div className={style.value}>
+                            <div className={style.data}>
+                                {Math.round(weatherServiceData?.weather?.main.temp || 0)}
+                            </div>
+                            <div className={style.detail}>
+                                &deg;C
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={style.secondary}>
+                    <div className={style.feelslike}>
+                        <div className={style.label}>
+                            But you think it is
+                        </div>
+                        <div className={style.value}>
+                            <div className={style.data}>
+                                {Math.round(weatherServiceData?.weather?.main.feels_like || 0)}
+                            </div>
+                            <div className={style.detail}>
+                                &deg;C
+                            </div>
+                        </div>
+                    </div>
+                    <div className={style.humidity}>
+                        <div className={style.label}>
+                            And humidity is
+                        </div>
+                        <div className={style.value}>
+                            <div className={style.data}>
+                                {Math.round(weatherServiceData?.weather?.main.humidity || 0)}
+                            </div>
+                            <div className={style.detail}>
+                                %
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={`${style.weatherDetail} ${isExpanded ? style.expanded : ''}`}>
+                <div className={style.collapsible__inner}>
+                    <div>This</div>
+                    <div>is</div>
+                    <div>details</div>
+                </div>
+            </div>
+        </div>
+    );
+}
