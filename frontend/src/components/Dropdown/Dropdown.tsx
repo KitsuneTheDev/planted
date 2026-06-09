d387cee977274fc5ee7dc7ad1f08593a51c6c1de
KitsuneTheDev <163762953+KitsuneTheDev@users.noreply.github.com>
2026-06-09 23:22:25 +0300

    update: create useDropdown hook and seperate Dropdown.tsx

--- frontend/src/components/Dropdown/Dropdown.tsx
+++ frontend/src/components/Dropdown/Dropdown.tsx
@@ -1,34 +1,14 @@
 import style from './Dropdown.module.css';
-import type { DropdownData } from '@planted/types';
-import { useState, useMemo } from 'react';
-import { debounceWrapper } from '../../utils/debounceWrapper';
-import { DropdownService } from '../../services/DropdownService';
+import { useDropdown } from './useDropdown';
 
 export default function Dropdown() {
-    const [dropdownData, setDropdownData] = useState<DropdownData[]>([]);
 
-    const search = useMemo(
-        () => debounceWrapper(async (query: string) => {
-                if(query.length < 2) {
-                    setDropdownData([]);
-                    return;
-                } else {
-                    const response = await DropdownService.getDropdownData(query);
-                    if(!response.isError) {
-                        setDropdownData(response.responseData.length === 0 ? [] : response.responseData);
-                        return;
-                    }
-                }
-            }, 500), []);
-
-    const handleDropdownChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        search(e.target.value);
-    }
+    const { dropdownData, handleDropdownChange } = useDropdown();
 
     return (
         <div className={`${style.dropdownOuter}`}>
             <div className={`${style.dropdownSearch}`}>
-                <input type="text" name="dropdown-input" placeholder='Set Location' id="dropdownInput" onChange={handleDropdownChange} />
+                <input type="text" name="dropdown-input" placeholder='Istanbul' id="dropdownInput" onChange={handleDropdownChange} />
             </div>
             <div className={`${style.dropdownDisplay} ${dropdownData.length === 0 ? style.hidden : null}`}>
                 {dropdownData.map((data, index) => {
