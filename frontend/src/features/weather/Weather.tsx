f7eb8847f681baee0ea85409cbf185d5ab5d107e
KitsuneTheDev <163762953+KitsuneTheDev@users.noreply.github.com>
2026-06-16 22:38:13 +0300

    Update Weather.tsx

--- frontend/src/features/weather/Weather.tsx
+++ frontend/src/features/weather/Weather.tsx
@@ -1,45 +1,45 @@
-import style from './Weather.module.css';
-import type { ReactElement } from "react";
-import { aqiMap } from '../../constants/aqiMap';
-import { useWeather } from './useWeather';
-import WeatherDetail from './WeatherDetail';
-import WeatherMain from './WeatherMain';
-import QualityMain from './QualityMain';
-import QualityDetail from './QualityDetail';
-import DayTime from './DayTime';
-import Grid from '../../components/Grid/Grid';
-import Row from '../../components/Row/Row';
+// import style from './Weather.module.css';
+// import type { ReactElement } from "react";
+// import { aqiMap } from '../../constants/aqiMap';
+// import { useWeather } from './useWeather';
+// import WeatherDetail from './WeatherDetail';
+// import WeatherMain from './WeatherMain';
+// import QualityMain from './QualityMain';
+// import QualityDetail from './QualityDetail';
+// import DayTime from './DayTime';
+// import Grid from '../../components/Grid/Grid';
+// import Row from '../../components/Row/Row';
 
-export default function Weather(): ReactElement {
+// export default function Weather(): ReactElement {
 
-    const { weatherServiceData } = useWeather();
-    console.log(weatherServiceData);
-    return(
-        <div className={style.contentWrapper}>
-            <Row>
-                <WeatherMain temp={Math.round(weatherServiceData?.weather?.main.temp || 0)} />
-            </Row>
-            <hr />
-            <Grid column={4}>
-                <WeatherDetail
-                    humidity={`${Math.round(weatherServiceData?.weather?.main.humidity || 0)} %`}
-                    feels={`${Math.round(weatherServiceData?.weather?.main.feels_like || 0)} °C`}
-                    wind={`${Math.round(weatherServiceData?.weather?.wind.speed || 0)} km/h`}
-                    aqi={`${aqiMap[weatherServiceData?.quality?.list[0]?.main.aqi]}`}
-                />
-            </Grid>
-            <hr />
-            <Row>
-                <QualityMain />
-            </Row>
-            <hr />
-            <Grid>
-                <QualityDetail />
-            </Grid>
-            <hr />
-            <Grid>
-                <DayTime />
-            </Grid>
-        </div>
-    );
-}
+//     const { weatherServiceData } = useWeather();
+//     console.log(weatherServiceData);
+//     return(
+//         <div className={style.contentWrapper}>
+//             <Row>
+//                 <WeatherMain temp={Math.round(weatherServiceData?.weather?.main.temp || 0)} />
+//             </Row>
+//             <hr />
+//             <Grid column={4}>
+//                 <WeatherDetail
+//                     humidity={`${Math.round(weatherServiceData?.weather?.main.humidity || 0)} %`}
+//                     feels={`${Math.round(weatherServiceData?.weather?.main.feels_like || 0)} °C`}
+//                     wind={`${Math.round(weatherServiceData?.weather?.wind.speed || 0)} km/h`}
+//                     aqi={`${aqiMap[weatherServiceData?.quality?.list[0]?.main.aqi]}`}
+//                 />
+//             </Grid>
+//             <hr />
+//             <Row>
+//                 <QualityMain />
+//             </Row>
+//             <hr />
+//             <Grid>
+//                 <QualityDetail />
+//             </Grid>
+//             <hr />
+//             <Grid>
+//                 <DayTime />
+//             </Grid>
+//         </div>
+//     );
+// }
