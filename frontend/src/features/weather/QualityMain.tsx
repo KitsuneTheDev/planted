60ba13d440c6337c4ab61f3d8b1e021c7f5010f5
KitsuneTheDev <163762953+KitsuneTheDev@users.noreply.github.com>
2026-06-20 21:44:38 +0300

    update: change aqiMap from svg to grid

--- frontend/src/features/weather/QualityMain.tsx
+++ frontend/src/features/weather/QualityMain.tsx
@@ -7,7 +7,9 @@ interface QualityMainProps {
 
 export default function QualityMain({aqi, aqiDetail}: QualityMainProps){
 
-    const markerPosition = 1 + ((aqi - 1) * 8) + ((aqi - 1) * 2) + 4;
+    const barLength = ((aqi - 1) * 20) + 10;
+    const markerPosition = `calc(${barLength}% - 4.5px)`;
+    console.log(markerPosition);
 
     return(
         <div className={style.contextWrapper}>
@@ -16,20 +18,14 @@ export default function QualityMain({aqi, aqiDetail}: QualityMainProps){
             </div>
             <div className={style.mainWrapper}>
                 <div className={style.aqiMap}>
-                    <svg viewBox='0 0 50 5' preserveAspectRatio='none'>
-                        <line x1="1" y1="3" x2="9" y2="3"  stroke="black" strokeWidth="10" strokeLinecap='round' />
-                        <line x1="1" y1="3" x2="9" y2="3"  stroke="#42ff58" strokeWidth="5" strokeLinecap='round' />
-                        <line x1="11" y1="3" x2="19" y2="3"  stroke="black" strokeWidth="10" strokeLinecap='round' />
-                        <line x1="11" y1="3" x2="19" y2="3"  stroke="#ffdc42" strokeWidth="5" strokeLinecap='round' />
-                        <line x1="21" y1="3" x2="29" y2="3"  stroke="black" strokeWidth="10" strokeLinecap='round' />
-                        <line x1="21" y1="3" x2="29" y2="3"  stroke="#ff8e42" strokeWidth="5" strokeLinecap='round' />
-                        <line x1="31" y1="3" x2="39" y2="3"  stroke="black" strokeWidth="10" strokeLinecap='round' />
-                        <line x1="31" y1="3" x2="39" y2="3"  stroke="#ff5842" strokeWidth="5" strokeLinecap='round' />
-                        <line x1="41" y1="3" x2="49" y2="3"  stroke="black" strokeWidth="10" strokeLinecap='round' />
-                        <line x1="41" y1="3" x2="49" y2="3"  stroke="#521515" strokeWidth="5" strokeLinecap='round' />
-                        <line x1={markerPosition} y1="3" x2={markerPosition} y2="3"  stroke="black" strokeWidth="15" strokeLinecap='round' />
-                        <line x1={markerPosition} y1="3" x2={markerPosition} y2="3"  stroke="white" strokeWidth="10" strokeLinecap='round' />
-                    </svg>
+                    <div className={style.aqiMapInner}>
+                        <div className={style.aqiMapBar}></div>
+                        <div className={style.aqiMapBar}></div>
+                        <div className={style.aqiMapBar}></div>
+                        <div className={style.aqiMapBar}></div>
+                        <div className={style.aqiMapBar}></div>
+                    </div>
+                    <div className={style.marker} style={{left: markerPosition}}></div>
                 </div>
                 <div className={style.aqiMain}>
                     <div className={style.mainContext}>
