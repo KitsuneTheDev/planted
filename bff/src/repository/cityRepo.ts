0ecb2fb52ce80a2b8bee173314efbc105b8ada82
KitsuneTheDev <163762953+KitsuneTheDev@users.noreply.github.com>
2026-07-03 16:57:40 +0300

    Update cityRepo.ts

--- bff/src/repository/cityRepo.ts
+++ bff/src/repository/cityRepo.ts
@@ -1,18 +1,18 @@
-// import City from "../db/models/City";
-// import { Op } from 'sequelize';
-// import type { InferAttributes, Model } from "sequelize";
+import City from "../db/models/City";
+import { Op } from 'sequelize';
+import type { InferAttributes, Model } from "sequelize";
 
-// export const getCityByName = async (query: string, page: number) => {
+export const getCityByName = async (query: string, page: number) => {
 
-//     const cityResponse: Array<InferAttributes<Model>> = await City.findAll({
-//         where: {
-//             city: {
-//                 [Op.iLike]: `${query}%`
-//             }
-//         },
-//         limit: 20,
-//         offset: 20*(page - 1),
-//     });
+    const cityResponse: Array<InferAttributes<Model>> = await City.findAll({
+        where: {
+            city: {
+                [Op.iLike]: `${query}%`
+            }
+        },
+        limit: 20,
+        offset: 20*(page - 1),
+    });
 
-//     return cityResponse;
-// }
+    return cityResponse;
+}
