e57d07a628c972750e1a900e86fc4cbaecc578a9
KitsuneTheDev <163762953+KitsuneTheDev@users.noreply.github.com>
2026-07-12 22:51:00 +0300

    Update plant.controller.ts

--- bff/src/controller/plant.controller.ts
+++ bff/src/controller/plant.controller.ts
@@ -1,19 +1,19 @@
-import type { NextFunction, Request, Response } from "express";
-import { findAllPlants } from "../service/plant.service";
+// import type { NextFunction, Request, Response } from "express";
+// import { findAllPlants } from "../service/plant.service";
 
-export async function fetchAllPlants(req: Request, res: Response, next: NextFunction) {
-    let page: number = Number.parseInt(String(req.query?.page) ?? 1);
+// export async function fetchAllPlants(req: Request, res: Response, next: NextFunction) {
+//     let page: number = Number.parseInt(String(req.query?.page) ?? 1);
 
-    if(isNaN(page) || !page) {
-        page = 1
-    }
+//     if(isNaN(page) || !page) {
+//         page = 1
+//     }
 
-    const results = await findAllPlants(page);
+//     const results = await findAllPlants(page);
 
-    if(!results) {
-        res.status(200).json([]);
-        return;
-    }
+//     if(!results) {
+//         res.status(200).json([]);
+//         return;
+//     }
 
-    res.status(200).json(results);
-}
+//     res.status(200).json(results);
+// }
