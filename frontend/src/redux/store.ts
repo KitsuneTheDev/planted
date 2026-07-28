a6d8aa7ba5aaed23bd365e6d4822403a0b73dfdb
KitsuneTheDev <163762953+KitsuneTheDev@users.noreply.github.com>
2026-07-28 19:26:38 +0300

    Update store.ts

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,16 +1,16 @@
-// import { configureStore } from '@reduxjs/toolkit';
-// import weatherServiceReducer from './slices/weatherServiceSlice';
-// import { plantApi } from './reduxApis/plantApi';
+import { configureStore } from '@reduxjs/toolkit';
+import weatherServiceReducer from './slices/weatherServiceSlice';
+import { plantApi } from './reduxApis/plantApi';
 
-// export type AppDispatch = typeof store.dispatch;
-// export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
 
-// const store = configureStore({
-//     reducer: {
-//         weatherServiceReducer,
-//         [plantApi.reducerPath]: plantApi.reducer,
-//     },
-//     middleware: (getDefault) => getDefault().concat(plantApi.middleware),
-// });
+const store = configureStore({
+    reducer: {
+        weatherServiceReducer,
+        [plantApi.reducerPath]: plantApi.reducer,
+    },
+    middleware: (getDefault) => getDefault().concat(plantApi.middleware),
+});
 
-// export default store;
+export default store;
