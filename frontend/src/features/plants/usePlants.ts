adc8f3e7db4ee5effbc31eb42ac376d59f7a2e1e
KitsuneTheDev <163762953+KitsuneTheDev@users.noreply.github.com>
2026-08-21 12:51:28 +0300

    Update usePlants.ts

--- frontend/src/features/plants/usePlants.ts
+++ frontend/src/features/plants/usePlants.ts
@@ -1,61 +1,61 @@
-import { useState, useMemo } from 'react';
-import { useGetAllPlantsDataQuery } from '../../redux/reduxApis/plantApi';
-import type { IPlant } from '@planted/types';
-import { useGetClimatologyDataQuery } from '../../redux/reduxApis/climatologyApi';
+// import { useState, useMemo } from 'react';
+// import { useGetAllPlantsDataQuery } from '../../redux/reduxApis/plantApi';
+// import type { IPlant } from '@planted/types';
+// import { useGetClimatologyDataQuery } from '../../redux/reduxApis/climatologyApi';
 
-export function usePlants() {
+// export function usePlants() {
 
-    const [page, setPage] = useState<number>(1);
+//     const [page, setPage] = useState<number>(1);
 
-    const {
-        data: plantData,
-        error: plantError,
-        isLoading: plantLoading,
-        isFetching: plantFetching,
-    } = useGetAllPlantsDataQuery(page);
+//     const {
+//         data: plantData,
+//         error: plantError,
+//         isLoading: plantLoading,
+//         isFetching: plantFetching,
+//     } = useGetAllPlantsDataQuery(page);
 
-    const {
-        data: climatologyData,
-        error: climatologyError,
-        isLoading: climatologyLoading,
-        isFetching: climatologyFetching,
-    } = useGetClimatologyDataQuery();
+//     const {
+//         data: climatologyData,
+//         error: climatologyError,
+//         isLoading: climatologyLoading,
+//         isFetching: climatologyFetching,
+//     } = useGetClimatologyDataQuery();
 
 
-    const isMore: boolean = (plantData?.length ?? 0) > 20;
-    const isFirstPage: boolean = page < 2;
+//     const isMore: boolean = (plantData?.length ?? 0) > 20;
+//     const isFirstPage: boolean = page < 2;
 
-    const plants: Array<IPlant> = useMemo(() => {
-        if(!plantData) return [];
-        return isMore ? plantData.slice(0, 20) : plantData;
-    }, [plantData, isMore]);
+//     const plants: Array<IPlant> = useMemo(() => {
+//         if(!plantData) return [];
+//         return isMore ? plantData.slice(0, 20) : plantData;
+//     }, [plantData, isMore]);
 
-    const handleNextClick = () => {
-        if(!isMore) return;
-        else {
-            setPage(prev => prev + 1);
-        }
-    }
+//     const handleNextClick = () => {
+//         if(!isMore) return;
+//         else {
+//             setPage(prev => prev + 1);
+//         }
+//     }
 
-    const handleBackClick = () => {
-        if(isFirstPage) return;
-        else{
-            setPage(prev => prev - 1);
-        }
-    }
+//     const handleBackClick = () => {
+//         if(isFirstPage) return;
+//         else{
+//             setPage(prev => prev - 1);
+//         }
+//     }
 
-    return { 
-        plants, 
-        plantError: plantError ? `Error occured: ${JSON.stringify(plantError)}` : null, 
-        plantLoading,
-        plantFetching,
-        isMore,
-        isFirstPage,
-        handleNextClick,
-        handleBackClick,
-        climatologyData,
-        climatologyError: climatologyError ? `Error occured: ${JSON.stringify(climatologyError)}` : null,
-        climatologyLoading,
-        climatologyFetching, 
-    };
-}
+//     return { 
+//         plants, 
+//         plantError: plantError ? `Error occured: ${JSON.stringify(plantError)}` : null, 
+//         plantLoading,
+//         plantFetching,
+//         isMore,
+//         isFirstPage,
+//         handleNextClick,
+//         handleBackClick,
+//         climatologyData,
+//         climatologyError: climatologyError ? `Error occured: ${JSON.stringify(climatologyError)}` : null,
+//         climatologyLoading,
+//         climatologyFetching, 
+//     };
+// }
